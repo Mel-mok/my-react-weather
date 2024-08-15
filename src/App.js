ad6004a9162741ad23eb1194d777238ddf7c716c
Melba Mokoena <Melmok2@gmail.com>
2024-08-15 06:52:20 +0200

    adjust css for components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
-import logo from "./logo.svg";
-import Weather from "./Weather";
+import React from "react";
+
+import Title from "./Title";
+import Header from "./Header";
+import Main from "./Main";
+
 import "./App.css";
-import "./Weather.css";
+import "./Title.css";
+import "./Header.css";
+import "./Main.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <section className="App">
+    <Title />
+    <div className="Weather-App">
+      <Header />
       <Main />
     </div>
+    </section>
   );
 }
 
